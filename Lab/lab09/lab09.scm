098d3ee5673be4ca2c3e3f3888cc51a2887d5f80
iceymoon07 <glacierluna@gmail.com>
2025-07-24 15:57:11 +0800

    lab09 finished

--- Lab/lab09/lab09.scm
+++ Lab/lab09/lab09.scm
@@ -1,19 +1,27 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) (if (< num1 num2) -1 (if (= num1 num2) 0 (if (> num1 num2) 1))))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) (lambda (inc) (+ inc num)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) (lambda (x) (f (g x))))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n) (if (> n 1)
+    (composed f (repeat f (- n 1)))
+    f))
 
 (define (max a b)
   (if (> a b)
-      a
-      b))
+    a
+    b))
 
 (define (min a b)
   (if (> a b)
-      b
-      a))
+    b
+    a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b)
+  (define bigger (max a b))
+  (define smaller (min a b))
+  (define remainder (modulo bigger smaller))
+  (if (zero? remainder)
+    smaller
+    (gcd smaller remainder)))
