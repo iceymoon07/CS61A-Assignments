0180364a9e79805bf1fc533bcf808b32449dd56a
iceymoon07 <glacierluna@gmail.com>
2025-07-30 15:32:30 +0800

    hw07 finished

--- HW/hw07/hw07.scm
+++ HW/hw07/hw07.scm
@@ -1,15 +1,21 @@
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp)
+  (if (= exp 1)
+    base
+    (if (even? exp)
+      (square (pow base (/ exp 2)))
+      (* (square (pow base (/ (- exp 1) 2))) base))))
 
 (define (repeatedly-cube n x)
   (if (zero? n)
-      x
-      (let (_________________)
-        (* y y y))))
+    x
+    (let
+      ((y (repeatedly-cube (- n 1) x)))
+      (* y y y))))
 
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cddr s)))
